5b7a9d16c4049576e9beee3e7c46e69ea271c504
dvlpr-denis <petlyuk.den@gmail.com>
2017-03-06 22:28:24 +0200

    create buttom

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+//add item function
+var addItem = function addItem(text) {
+	var item = $('<li class="todo-list-item">\n\t\t\t\t\t\t\t\t\t\t' + text + ' \n\t\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t\t<div class="tresh">\n            \t\t\t\t\t<i class="fa fa-trash-o" aria-hidden="true"></i>\n            \t\t\t\t</div>\n            \n\t\t\t\t\t\t\t\t\t\t<div class="complete">\n\t\t\t\t\t\t\t\t\t\t\t<img src="img/complete.png" alt="">\n\t\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t</li>'),
+	    todoList = $('.todo-list');
+
+	todoList.append(item);
+};
+
 // get value in input on click and create li-item
 
 
@@ -9,10 +17,3 @@ $('.add').on('click', function () {
 
 	if (valueItem !== '' && valueItem != null) addItem(valueItem);
 });
-
-var addItem = function addItem(text) {
-	var item = $('<li class="todo-list-item"> ' + text + ' </li>'),
-	    todoList = $('.todo-list');
-
-	todoList.append(item);
-};
